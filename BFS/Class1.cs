3e2d2b529f465f1dec7ca0968f476fe57518b367
Shannon <917748823@qq.com>
2016-08-20 00:02:03 +0800

    BFS 实现代码以及对应的UT

---
+++ BFS/Class1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    public class BFSSearch<T>
+    {
+        private T[,] m_Map;
+        private Func<T, bool> m_GridAvailableHandle;
+        private int m_RowCount;
+        private int m_ColCount;
+
+        public BFSSearch(T[,] map, Func<T, bool> gridAvailableHandle)
+        {
+            m_Map = map;
+            m_RowCount = map.GetLength(0);
+            m_ColCount = map.GetLength(1);
+            m_GridAvailableHandle = gridAvailableHandle;
+        } 
+
+        public List<IPoint> FindPath(int startX, int startY, int endX, int endY)
+        {
+            var startPoint = new Point(startX, startY);
+            var endPoint = new Point(endX, endY);
+            bool[,] color = new bool[m_RowCount, m_ColCount];
+            int[,] distance = new int[m_RowCount, m_ColCount];
+            ResetDistance(distance, startPoint);
+            
+            var result=new List<IPoint>();
+            var queue = new Queue<Point>();
+            queue.Enqueue(startPoint);
+
+            while (queue.Count>0)
+            {
+                var current = queue.Dequeue();
+                //不能是已经染色的
+                if (color[current.X, current.Y])
+                {
+                    continue;
+                }
+
+                //进行染色
+                color[current.X, current.Y] = true;
+
+                //是否是目标
+                if (IsReachTarget(current, endPoint))
+                {
+                    while (current.Pre!=null)
+                    {
+                        result.Add(current);
+                        current = current.Pre;
+                    }
+
+                    //根节点也要添加进去
+                    result.Add(current);
+                    break;
+                }
+
+                //4个方向添加栅格
+                EnqueueNextPoint(current, current.X - 1, current.Y, color, queue, distance);
+                EnqueueNextPoint(current, current.X + 1, current.Y, color, queue, distance);
+                EnqueueNextPoint(current, current.X, current.Y - 1, color, queue, distance);
+                EnqueueNextPoint(current, current.X, current.Y + 1, color, queue, distance);
+            }
+            result.Reverse();
+            return result;
+        }
+
+        private bool IsReachTarget(Point current, Point endPoint)
+        {
+            return current.X == endPoint.X && current.Y == endPoint.Y;
+        }
+
+        private void EnqueueNextPoint(Point current, int nextX,int nextY,bool[,] color, Queue<Point> queue,
+            int[,] distance)
+        {
+            if (IsPositionValid(nextX, nextY))
+            {
+                var nextPoint = new Point(nextX, nextY);
+                //下个节点需要未被染色的以及可达的
+                if (!color[nextPoint.X, nextPoint.Y]&&m_GridAvailableHandle(m_Map[nextPoint.X, nextPoint.Y]))
+                {
+                    queue.Enqueue(nextPoint);
+
+                    //如果新的距离小于之前的距离，那么重新刷新距离和pre节点
+                    if (distance[current.X, current.Y] + 1 < distance[nextPoint.X, nextPoint.Y])
+                    {
+                        distance[nextPoint.X, nextPoint.Y] = distance[current.X, current.Y] + 1;
+                        nextPoint.Pre = current;
+                    }
+                }
+            }
+        }
+
+        private bool IsPositionValid(int x, int y)
+        {
+            return x >= 0 && x < m_RowCount && y >= 0 && y < m_ColCount;
+        }
+
+        private void ResetDistance(int[,] distance, Point startPoint)
+        {
+            for (int i = 0; i <m_RowCount ; i++)
+            {
+                for (int j = 0; j < m_ColCount; j++)
+                {
+                    distance[i, j] = int.MaxValue;
+                }
+            }
+
+            distance[startPoint.X, startPoint.Y] = 0;
+        }
+    }
+
+    internal class Point : IPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+        public Point Pre { set; get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public interface IPoint
+    {
+        int X { set; get; }
+        int Y { set; get; }
+    }
+}
---
+++ Test/Class1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BFS;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        private static int[,] GetMap(string filePath)
+        {
+            int width = 0;
+            int height = 0;
+            int[,] map = null;
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    using (var fs = new FileStream(filePath, FileMode.Open))
+                    {
+                        using (var sr = new StreamReader(fs))
+                        {
+                            var line = sr.ReadLine();
+                            var headLine = line.Split(',');
+                            height = int.Parse(headLine[0]);
+                            width = int.Parse(headLine[1]);
+                            map = new int[height, width];
+
+                            int index = 0;
+                            while (!string.IsNullOrEmpty(line))
+                            {
+                                line = sr.ReadLine();
+                                for (int i = 0; i < line.Length; i++)
+                                {
+                                    map[index,i] = int.Parse(line[i].ToString());
+                                }
+                                index++;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return map;
+        }
+
+        private static bool IsGridAvailable(int value)
+        {
+            return value != 1;
+        }
+
+        private static void ArraryEqual(List<IPoint> a, List<Tuple<int,int>> b)
+        {
+            Assert.AreEqual(a.Count, b.Count);
+            for (int i = 0; i < a.Count; i++)
+            {
+                Assert.AreEqual(a[i].X, b[i].Item1);
+                Assert.AreEqual(a[i].Y, b[i].Item2);
+            }
+        }
+
+        [Test]
+        public void First2EndTest()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMap", "1.txt");
+            var map = GetMap(filePath);
+            int startX = 0;
+            int startY = 0;
+            int endX = 0;
+            int endY = 3;
+
+            BFSSearch<int> bfsSearch=new BFSSearch<int>(map, IsGridAvailable);
+            var result= bfsSearch.FindPath(startX, startY, endX, endY);
+
+            Assert.AreEqual(result.Count, 10);
+            ArraryEqual(result,new List<Tuple<int, int>>()
+            {
+               new Tuple<int, int>(0,0),
+               new Tuple<int, int>(1,0),
+               new Tuple<int, int>(2,0),
+               new Tuple<int, int>(3,0),
+               new Tuple<int, int>(3,1),
+               new Tuple<int, int>(3,2),
+               new Tuple<int, int>(3,3),
+               new Tuple<int, int>(2,3),
+               new Tuple<int, int>(1,3),
+               new Tuple<int, int>(0,3)
+            });
+        }
+
+        [Test]
+        public void CannotReachTest()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMap", "2.txt");
+            var map = GetMap(filePath);
+            int startX = 0;
+            int startY = 0;
+            int endX = 0;
+            int endY = 3;
+
+            BFSSearch<int> bfsSearch = new BFSSearch<int>(map, IsGridAvailable);
+            var result = bfsSearch.FindPath(startX, startY, endX, endY);
+
+            Assert.AreEqual(result.Count, 0);
+            ArraryEqual(result, new List<Tuple<int, int>>()
+            {
+            });
+        }
+
+        [Test]
+        public void SingleRowTest()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMap", "3.txt");
+            var map = GetMap(filePath);
+            int startX = 0;
+            int startY = 0;
+            int endX = 0;
+            int endY = 3;
+
+            BFSSearch<int> bfsSearch = new BFSSearch<int>(map, IsGridAvailable);
+            var result = bfsSearch.FindPath(startX, startY, endX, endY);
+
+            Assert.AreEqual(result.Count, 4);
+            ArraryEqual(result, new List<Tuple<int, int>>()
+            {
+                  new Tuple<int, int>(0,0),
+                  new Tuple<int, int>(0,1),
+                  new Tuple<int, int>(0,2),
+                  new Tuple<int, int>(0,3)
+            });
+        }
+
+        [Test]
+        public void SingleColTest()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMap", "4.txt");
+            var map = GetMap(filePath);
+            int startX = 0;
+            int startY = 0;
+            int endX = 3;
+            int endY = 0;
+
+            BFSSearch<int> bfsSearch = new BFSSearch<int>(map, IsGridAvailable);
+            var result = bfsSearch.FindPath(startX, startY, endX, endY);
+
+            Assert.AreEqual(result.Count, 4);
+            ArraryEqual(result, new List<Tuple<int, int>>()
+            {
+                  new Tuple<int, int>(0,0),
+                  new Tuple<int, int>(1,0),
+                  new Tuple<int, int>(2,0),
+                  new Tuple<int, int>(3,0)
+            });
+        }
+
+
+        [Test]
+        public void MutilTest()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMap", "5.txt");
+            var map = GetMap(filePath);
+            int startX = 0;
+            int startY = 2;
+            int endX = 2;
+            int endY = 1;
+
+            BFSSearch<int> bfsSearch = new BFSSearch<int>(map, IsGridAvailable);
+            var result = bfsSearch.FindPath(startX, startY, endX, endY);
+
+            Assert.AreEqual(result.Count, 10);
+            ArraryEqual(result, new List<Tuple<int, int>>()
+            {
+                  new Tuple<int, int>(0,2),
+                  new Tuple<int, int>(0,3),
+                  new Tuple<int, int>(0,4),
+                  new Tuple<int, int>(1,4),
+                  new Tuple<int, int>(2,4),
+                  new Tuple<int, int>(2,3),
+                  new Tuple<int, int>(3,3),
+                  new Tuple<int, int>(3,2),
+                  new Tuple<int, int>(3,1),
+                  new Tuple<int, int>(2,1),
+            });
+        }
+    }
+}
